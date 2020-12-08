24d072be0e7a50ba0809c47bad799eaf15d4c4cc
Daiyan <62832590+Andaloupy@users.noreply.github.com>
2020-12-08 11:11:56 +0100

    Create avl.h

---
+++ avl.h
@@ -0,0 +1,16 @@
+#ifndef avl_h
+#define avl_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "structures.h"
+
+int tree_depth(Tree* tree);
+int bf(Tree* tree);
+void right_rotation(Tree** tree);
+void left_rotation(Tree** tree);
+void balance(Tree** tree);
+void print_tree(Tree* tree);
+
+#endif // avl_h
