2b496efffb431fdc9e94843d3f259f6b35fb93c1
Daiyan <62832590+Andaloupy@users.noreply.github.com>
2020-12-08 11:08:57 +0100

    Update Huffman.h

--- Huffman.h
+++ Huffman.h
@@ -1,28 +1,34 @@
+#ifndef Huffman_h
+#define Huffman_h
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-typedef struct List{
-    int data;
-    char c;
-    struct List* next;
-}List; 
+#include "structures.h"
+#include "avl.h"
+
+
 
 void output(char*file);
 int countchar(char*file);
 List* list_carac(char* file);
-List* create_element(char c);
+
 void print_list(List* l);
 void list_remove(List** l, int n);
 List* smallest(List *l);
 
-typedef struct Tree{
-    int poids;
-    char c;
-    struct Tree* left;
-    struct Tree* right;
-}Tree;
 
-Tree* create_element_T();
+
 Tree* create_huffman(List* element);
-void Dico(Tree* root);
+void Dico(Tree* root, char* s, FILE* dico);
 
+void print_tree(Tree* tree);
+
+void concatenate(char* s, char bit);
+void decon(char* s);
+void translate(FILE* dico, char* s);
+void compress_file(char* name);
+
+#endif // Huffman_h
