1c22ec3b871ff61c93eda4b9c236d17e4245d10b
Daiyan <62832590+Andaloupy@users.noreply.github.com>
2020-12-08 11:08:25 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,36 +2,22 @@
 #include <stdlib.h>
 #include "Huffman.h"
 #include <string.h>
+#include <time.h>
 
 int main(){
-    int test;
-    output("Alice.txt");
-    test = countchar("Alice.txt");
-    printf("%d\n",test);
-    test = countchar("Output.txt");
-    printf("%d\n",test);
+    clock_t t1, t2;
+    char s[10000];
+    printf("enter the name of your file :");
+    scanf("%s", s);
+    t1 = clock();
 
-    List* list_car;
-    list_car = list_carac("Alice.txt");
+    compress_file(s);
 
-    List* sorted_list, *temp;
-    sorted_list = smallest(list_car);
-    list_remove((&list_car), sorted_list->data);
-    temp = sorted_list;
-    while (list_car->next != NULL){
-        temp->next = smallest(list_car);
-        list_remove((&list_car), temp->next->data);
-        temp = temp->next;
-    }
-    temp->next = list_car; 
-    list_remove((&list_car), temp->data);
-    print_list(sorted_list);
-
-    Tree* root;
-    root = create_huffman(sorted_list);
-    printf("%d\n", root->poids);
-    Dico(root);
-    printf("%d\n", root->poids);
-
+    t2=clock();
+    double temps = (t2-t1);
+    printf("\nCompressed in %f seconds\n", temps/1000);
+    //printf("%ld\n", t1);
+    //printf("%ld", t2);
     return 0;
 }
+
